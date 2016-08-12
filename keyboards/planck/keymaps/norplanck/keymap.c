2c0316ca73a2f1c96cd50d2d319fa4e5bc28f49c
Lance <ltjessem@gmail.com>
2016-08-12 10:33:58 +0200

    .

--- keyboards/planck/keymaps/norplanck/keymap.c
+++ keyboards/planck/keymaps/norplanck/keymap.c
@@ -28,6 +28,7 @@ extern keymap_config_t keymap_config;
 #define M_TIDL 6        // AltGR + ¨, space = ~
 #define M_CART 7        // Shift + ¨, space = ^
 #define M_LOCK 8	// Win + L
+#define M_GRV 9		// Shift + \, space = `
 #define EXT_PLV M(13)
 #define TOG_OUT M(14)
 
@@ -50,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC},
-  {KC_BSPC, KC_A,    KC_S,    KC_D, KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,SFT_T(KC_ENT)},
-  {KC_LCTL, KC_LGUI, KC_LALT, L3,   LOWER,  KC_SPC, KC_SPC, RAISE,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT}
+  {KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC},
+  {KC_BSPC, KC_A,    KC_S,    KC_D, KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT)},
+  {KC_LCTL, KC_LGUI, KC_LALT, L3,   LOWER,  KC_SPC, KC_SPC, RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
@@ -87,14 +88,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = {
   {KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,           KC_0,       KC_EQL},
-  {_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  M(M_LOCK),        M(M_CART),  M(M_TIDL)},
+  {_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  M(M_LOCK),      M(M_CART),  M(M_TIDL)},
   {KC_CAPS,  KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______,  KC_NUBS,  LSFT(KC_NUBS),  KC_NUHS,    XXXXXXX},
   {_______,  _______,  _______,  _______,  _______,  KC_INS,   KC_INS,   _______,  KC_MUTE,  KC_VOLD,        KC_VOLU,    _______}
 },  
 
 /* Layer 3, mostly AltGr stuff. 
  * ,-----------------------------------------------------------------------------------.
- * |  |   |      |   @  |   £  |      |      |      |   {  |   [  |   ]  |   }  |      |
+ * |   |  |      |   @  |   £  |      |      |      |   {  |   [  |   ]  |   }  |   `  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |   €  |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -104,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_L3] = {
-  {KC_GRV, _______, RALT(KC_2), RALT(KC_3), _______, _______, _______, RALT(KC_7),  RALT(KC_8), RALT(KC_9), RALT(KC_0), _______},
+  {KC_GRV,  _______, RALT(KC_2), RALT(KC_3), _______, _______, _______, RALT(KC_7),  RALT(KC_8), RALT(KC_9), RALT(KC_0), M(M_GRV)},
   {_______, _______, _______,    RALT(KC_E), _______, _______, _______, _______,     _______,    _______,    _______,    _______},
   {_______, _______, _______,    _______,    _______, _______, _______, RALT(KC_M),  _______,    _______,    _______,    _______},
   {_______, _______, _______,    _______,    _______, _______, _______, _______,     _______,    _______,    _______,    _______}
@@ -239,6 +240,12 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 	case M_LOCK:
 		if (record->event.pressed) {
 		return MACRO( D(LGUI), T(L), U(LGUI), END );
+	}
+	break;
+
+	case M_GRV:
+		if (record->event.pressed) {
+		return MACRO( D(RSFT), T(EQL), U(RSFT), T(SPACE), END );
 	}
 	break;
 
